ff1b213e7a7184613eb8be373db879513b226517
MVandaglas <martijn.wissink@vandaglas.com>
2024-10-18 12:10:03 +0200

    Update PricePilot.py

--- PricePilot.py
+++ PricePilot.py
@@ -64,15 +64,11 @@ if st.button("Verstuur chat met GPT"):
             matched_article_number = fuzzy_match_synonyms(customer_input, synonym_dict)
             if matched_article_number:
                 article_number, description = find_article_details(matched_article_number)
-                if article_number and description:
-                    st.write(f"Bedoelt u artikelnummer {article_number}, {description}?")
-                    verification = st.radio("Klopt dit artikelnummer?", ("Ja", "Nee"))
-                    if verification == "Nee":
-                        st.write("Gelieve meer informatie te geven om het juiste artikelnummer te vinden.")
+               
                     else:
                         st.session_state.chat_history.append({"role": "user", "content": customer_input})
                         response = openai.chat.completions.create(
-                            model="gpt-3.5-turbo",
+                               model="gpt-3.5-turbo",
                             messages=[{"role": chat["role"], "content": chat["content"]} for chat in st.session_state.chat_history],
                             max_tokens=150
                         )
