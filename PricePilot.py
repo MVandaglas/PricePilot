15c8fd023a862bc194723dd6d03d3942129eee2f
MVandaglas <martijn.wissink@vandaglas.com>
2024-10-18 15:10:08 +0200

    Update PricePilot.py

--- PricePilot.py
+++ PricePilot.py
@@ -36,8 +36,6 @@ article_table = pd.DataFrame(article_table)
 st.sidebar.title("PricePilot - Klantprijsassistent")
 st.sidebar.write("Dit is een tool voor het genereren van klant specifieke prijzen op basis van ingevoerde gegevens.")
 
-
-
 if guestimate_offer_size > 50000:
     estimated_size_category = 4
 elif guestimate_offer_size > 25000:
