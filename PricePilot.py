f6f6ee1b92561320ba419ae808c025f4de23b0dd
MVandaglas <martijn.wissink@vandaglas.com>
2024-10-21 09:52:49 +0200

    Update PricePilot.py

--- PricePilot.py
+++ PricePilot.py
@@ -22,7 +22,7 @@ customer_data = {
 
 # Initialiseer offerte DataFrame en klantnummer in sessiestatus
 if "offer_df" not in st.session_state:
-    st.session_state.offer_df = pd.DataFrame(columns=["Artikelnaam", "Artikelnummer", "Breedte", "Hoogte", "Aantal", "M2 totaal"])
+    st.session_state.offer_df = pd.DataFrame(columns=["Artikelnaam", "Artikelnummer", "Breedte", "Hoogte", "Aantal", "RSP", "M2 p/s", "M2 totaal"])
 if "customer_number" not in st.session_state:
     st.session_state.customer_number = ""
 
