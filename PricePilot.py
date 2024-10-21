a5d4224a17c1eae8d50421d299b8eaf63e6f86d4
MVandaglas <martijn.wissink@vandaglas.com>
2024-10-21 13:09:54 +0200

    Update PricePilot.py

--- PricePilot.py
+++ PricePilot.py
@@ -269,11 +269,9 @@ def generate_pdf(df):
 
     elements.append(table)
 
-    # Voeg drie lege regels toe
-        elements.append(Paragraph(""))
+    # Voeg drie lege regels toe    elements.append(Paragraph(""))
     elements.append(Paragraph(""))
-    elements.append(Paragraph(""))
-    from reportlab.lib.enums import TA_RIGHT
+    elements.append(Paragraph(""))from reportlab.lib.enums import TA_RIGHT
 
     right_aligned_style = styles['Normal'].clone('right_aligned')
     right_aligned_style.alignment = TA_RIGHT
