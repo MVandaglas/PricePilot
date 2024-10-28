350dc0a8dc830709efa87dfccac0ee511d77fd28
MVandaglas <martijn.wissink@vandaglas.com>
2024-10-28 11:46:21 +0100

    Update PricePilot.py

--- PricePilot.py
+++ PricePilot.py
@@ -374,7 +374,7 @@ if selected_tab == "Offerte Genereren":
             st.session_state.offer_df = edited_df
 
 # Opgeslagen Offertes tab
-st.write(elst.write(f"<div style='position: fixed; bottom: 10px; right: 10px; font-size: 10px;'>Offertenummer: {st.session_state.next_offer_number}</div>", unsafe_allow_html=True)
+st.write(f"<div style='position: fixed; bottom: 10px; right: 10px; font-size: 10px;'>Offertenummer: {st.session_state.next_offer_number}</div>", unsafe_allow_html=True)
 
 if selected_tab == "Opgeslagen Offertes":
     st.title("Opgeslagen Offertes")
