e8630baec13ff1020db0c603f1d007c4cc3da508
MVandaglas <martijn.wissink@vandaglas.com>
2024-10-28 10:25:37 +0100

    Update PricePilot.py

--- PricePilot.py
+++ PricePilot.py
@@ -342,27 +342,20 @@ if selected_tab == "Offerte Genereren":
             st.download_button(label="Download PDF", data=pdf_buffer, file_name="offerte.pdf", mime="application/pdf")
 
         # Voeg een knop toe om de artikelen op te slaan in het geheugen
-        if st.button("Sla offerte op", key='save_offerte_button'):
-            # Genereer een uniek offertenummer
-            if 'next_offer_number' not in st.session_state:
-                if not st.session_state.saved_offers.empty:
-                    st.session_state.next_offer_number = int(st.session_state.saved_offers['Offertenummer'].max()) + 1
-                elif not st.session_state.offer_df.empty:
-                    st.session_state.next_offer_number = int(st.session_state.offer_df['Offertenummer'].max()) + 1
-                else:
-                    st.session_state.next_offer_number = 1
-            offer_number = st.session_state.next_offer_number
-            st.session_state.offer_df['Offertenummer'] = offer_number
-            st.session_state.next_offer_number += 1
-            if 'next_offer_number' not in st.session_state:
-    if not st.session_state.saved_offers.empty:
-        st.session_state.next_offer_number = int(st.session_state.saved_offers['Offertenummer'].max()) + 1
-    elif not st.session_state.offer_df.empty:
-        st.session_state.next_offer_number = int(st.session_state.offer_df['Offertenummer'].max()) + 1
-    else:
-        st.session_state.next_offer_number = 1
-            offer_number = st.session_state.next_offer_number
-            st.session_state.next_offer_number += 1
+if st.button("Sla offerte op", key='save_offerte_button'):
+    # Genereer een uniek offertenummer
+    if 'next_offer_number' not in st.session_state:
+        if not st.session_state.saved_offers.empty:
+            st.session_state.next_offer_number = int(st.session_state.saved_offers['Offertenummer'].max()) + 1
+        elif not st.session_state.offer_df.empty:
+            st.session_state.next_offer_number = int(st.session_state.offer_df['Offertenummer'].max()) + 1
+        else:
+            st.session_state.next_offer_number = 1
+
+    offer_number = st.session_state.next_offer_number
+    st.session_state.offer_df['Offertenummer'] = offer_number
+    st.session_state.next_offer_number += 1
+
 
             # Bereken eindtotaal
             if all(col in edited_df.columns for col in ['RSP', 'M2 totaal']):
