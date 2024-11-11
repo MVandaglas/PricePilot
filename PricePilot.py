8708a3139c78e2b15c3936541ef43f17322f6b80
MVandaglas <martijn.wissink@vandaglas.com>
2024-11-11 15:02:18 +0100

    Update PricePilot.py

--- PricePilot.py
+++ PricePilot.py
@@ -10,6 +10,8 @@ import re
 from datetime import datetime
 from st_aggrid import AgGrid
 
+client = OpenAI()
+
 # OpenAI API-sleutel instellen
 api_key = os.getenv("OPENAI_API_KEY")
 if not api_key:
@@ -183,7 +185,7 @@ def handle_gpt_chat():
                         ])
             else:
                 # Gebruik GPT om te proberen ontbrekende details te vinden
-                response = openai.chat.completions.create(
+                response = client.chat.completions.create(
                     model="gpt-4",
                     messages=[
                         {"role": "system", "content": "Je bent een offerte generatie assistent in de b2b glaswereld. Je krijgt klantverzoeken binnen die glassamenstellingen bevatten. Dit zal zeer waarschijnlijk een combinatie zijn van aantal, de desbetreffende samenstelling met productkenmerken, breedte en hoogte."},
