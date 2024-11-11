638752908a598095b01598c0ffaf716bad4515c4
MVandaglas <martijn.wissink@vandaglas.com>
2024-11-11 14:40:57 +0100

    Update PricePilot.py

--- PricePilot.py
+++ PricePilot.py
@@ -367,7 +367,7 @@ if selected_tab == "Offerte Genereren":
     # Toon bewaarde offerte DataFrame in het middenscherm en maak het aanpasbaar
     if st.session_state.offer_df is not None and not st.session_state.offer_df.empty:
         st.title("Offerteoverzicht")
-        edited_df = AgGrid(st.session_state.offer_df, editable=True, fit_columns_on_grid_load=True, theme='light')['data']
+        edited_df = AgGrid(st.session_state.offer_df, editable=True, fit_columns_on_grid_load=True, theme='streamlit')['data']
 
 # Voeg een knop toe om de offerte als PDF te downloaden
 if st.button("Download offerte als PDF", key='download_pdf_button'):
