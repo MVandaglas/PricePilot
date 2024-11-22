9eed770f62d98693ed5096614e7d7125feb017d7
MVandaglas <martijn.wissink@vandaglas.com>
2024-11-22 14:54:59 +0100

    Update PricePilot.py

--- PricePilot.py
+++ PricePilot.py
@@ -474,13 +474,14 @@ if 'Rijnummer' not in st.session_state.offer_df.columns:
     # Maak grid-opties aan voor AgGrid zonder gebruik van JsCode
 gb = GridOptionsBuilder.from_dataframe(st.session_state.offer_df)
 gb.configure_default_column(flex=1, min_width=100, editable=True)
+gb.configure_column("Artikelnaam", width=200)  # Stel de kolombreedte van Artikelnaam in op 150 pixels
 gb.configure_column("Offertenummer", hide=True)
 gb.configure_column("Breedte", editable=True, type=["numericColumn"])
 gb.configure_column("Hoogte", editable=True, type=["numericColumn"])
 gb.configure_column("Aantal", editable=True, type=["numericColumn"])
 gb.configure_column("RSP", editable=True, type=["numericColumn"])
 gb.configure_selection('multiple', use_checkbox=True)
-gb.configure_grid_options(domLayout='normal', rowHeight=22)  # Dit zorgt ervoor dat scrollen mogelijk is
+gb.configure_grid_options(domLayout='normal', rowHeight=23)  # Dit zorgt ervoor dat scrollen mogelijk is
 
 grid_options = gb.build()
 
