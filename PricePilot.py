b3c502c8177a13de657c78eed28416c37e85494b
MVandaglas <martijn.wissink@vandaglas.com>
2024-12-02 13:48:35 +0100

    Update PricePilot.py

--- PricePilot.py
+++ PricePilot.py
@@ -258,9 +258,8 @@ else:
     st.session_state.selected_rows = []
 
 
-
 def delete_selected_rows(df, selected_rows):
-    if 'selected_rows' is not None and len(selected_rows) > 0:
+    if selected_rows is not None and len(selected_rows) > 0:
         # Verwijder de geselecteerde rijen en reset de index
         df = df.drop(index=selected_rows_id, errors='ignore').reset_index(drop=True)
     return df
