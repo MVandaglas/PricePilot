874165539cad473e92c3d5c2b41dd853dd1e413f
MVandaglas <martijn.wissink@vandaglas.com>
2024-12-02 13:36:16 +0100

    Update PricePilot.py

--- PricePilot.py
+++ PricePilot.py
@@ -282,9 +282,10 @@ with col1:
 with col2:
     if st.button("Verwijder geselecteerde rijen", key='delete_rows_button'):
         selected = st.session_state.get('selected_rows', [])
+        
     if selected is None or not isinstance(selected, list):
         selected = []
-    st.write("Geselecteerde rijen (debug informatie):", selected)
+        st.write("Geselecteerde rijen (debug informatie):", selected)
     
     # Controleer of 'selected' een geldige lijst is en converteer naar integers indien nodig
     if isinstance(selected, list) and len(selected) > 0:
