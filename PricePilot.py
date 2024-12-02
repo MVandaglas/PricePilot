438eecfd5f6ad3aeac76a0dd4ee16c332e0a3cff
MVandaglas <martijn.wissink@vandaglas.com>
2024-12-02 10:29:16 +0100

    Update PricePilot.py

--- PricePilot.py
+++ PricePilot.py
@@ -217,7 +217,7 @@ with col1:
 
 with col2:
     if st.button("Verwijder geselecteerde rijen", key='delete_rows_button'):
-        selected = response['selected_rows'] if 'selected_rows' in response and response['selected_rows'] is not None else []
+        selected = st.session_state.selected_rows if 'selected_rows' in st.session_state else []
         st.write("Geselecteerde rijen (debug informatie):", selected)
         if selected is not None and len(selected) > 0:
             st.session_state.offer_df = delete_selected_rows(st.session_state.offer_df, selected)
