ac831d63fc56c7ac95cb636d78b1ddecf71154a6
MVandaglas <martijn.wissink@vandaglas.com>
2024-12-02 14:11:34 +0100

    Update PricePilot.py

--- PricePilot.py
+++ PricePilot.py
@@ -287,9 +287,9 @@ with col2:
         st.write("Geselecteerde rijen (debug informatie):", selected)
 
         # Controleer of 'selected' een geldige lijst is en voer verwijderactie uit
-        
-            if not selected.empty:            st.session_state.offer_df = delete_selected_rows(st.session_state.offer_df, selected_rows)
-                        st.session_state.selected_rows = []  # Reset de geselecteerde rijen na verwijderen
+        if len(selected) > 0:
+            st.session_state.offer_df = delete_selected_rows(st.session_state.offer_df, selected)
+            st.session_state.selected_rows = []  # Reset de geselecteerde rijen na verwijderen
         else:
             st.warning("Selecteer eerst rijen om te verwijderen.")
 
