5194a9de1418791bfa6b7aaba93f9c09314fe602
MVandaglas <martijn.wissink@vandaglas.com>
2024-12-02 11:16:58 +0100

    Update PricePilot.py

--- PricePilot.py
+++ PricePilot.py
@@ -207,42 +207,40 @@ def update_rsp_for_all_rows(df, prijsscherpte):
 # Functie om geselecteerde rijen te verwijderen
 def delete_selected_rows(df, selected_rows):
     if selected_rows is not None and len(selected_rows) > 0:
-        df = df.drop(selected_rows).reset_index(drop=True)
+        # Controleer of de indices in selected_rows geldig zijn voor het DataFrame
+        valid_indices = [idx for idx in selected_rows if idx in df.index]
+        if len(valid_indices) != len(selected_rows):
+            print("Waarschuwing: Een of meer geselecteerde rijen bestaan niet in het DataFrame.")
+        df = df.drop(valid_indices).reset_index(drop=True)
     return df
 
-# Configuratie voor AgGrid
+# Maak grid-opties aan voor AgGrid zonder gebruik van JsCode
 gb = GridOptionsBuilder.from_dataframe(st.session_state.offer_df)
-gb.configure_selection(selection_mode="multiple", use_checkbox=True, pre_selected_rows=[])
-gb.configure_default_column(editable=True, resizable=True)
+gb.configure_default_column(flex=1, min_width=100, editable=True)
+gb.configure_column("Rijnummer", editable=False, cellStyle={"backgroundColor": "#f5f5f5"})
+gb.configure_column("Artikelnaam", width=400)  # Stel de kolombreedte van Artikelnaam in op 400 pixels
+gb.configure_column("Offertenummer", hide=True)
+gb.configure_column("Breedte", editable=True, type=["numericColumn"])
+gb.configure_column("Hoogte", editable=True, type=["numericColumn"])
+gb.configure_column("Aantal", editable=True, type=["numericColumn"])
+gb.configure_column("RSP", editable=False, type=["numericColumn"], cellStyle={"backgroundColor": "#f5f5f5"})
+gb.configure_selection('multiple', use_checkbox=True, pre_selected_rows=[])  # Gebruik checkboxes voor selectie
+gb.configure_grid_options(domLayout='normal', rowHeight=23)  # Dit zorgt ervoor dat scrollen mogelijk is
+
 grid_options = gb.build()
 
 # Toon de AG Grid met het material-thema
-response = AgGrid(
+edited_df_response = AgGrid(
     st.session_state.offer_df,
     gridOptions=grid_options,
     theme='material',
     fit_columns_on_grid_load=True,
     enable_enterprise_modules=True,
-    update_mode=GridUpdateMode.MANUAL,
+    update_mode='SELECTION_CHANGED',  # Update als selectie verandert
     columns_auto_size_mode=ColumnsAutoSizeMode.FIT_CONTENTS,
-    data_return_mode=DataReturnMode.AS_INPUT,
+    data_return_mode=DataReturnMode.FILTERED,
+    allow_unsafe_jscode=True  # Sta het gebruik van onveilige JsCode toe om de selectie juist te maken
 )
-
-# Sla de geselecteerde rijen op in sessie status
-# Debugging: print de response van de AgGrid
-st.write("Debug - Type AgGrid response:", type(response))
-st.write("Debug - Full AgGrid response:", response)
-if 'selected_rows' in response and response['selected_rows'] is not None:
-    st.write("Debug - Type selected_rows:", type(response['selected_rows']))
-    st.write("Debug - Length of selected_rows:", len(response['selected_rows']))
-    st.write("Debug - Content of selected_rows:", response['selected_rows'])
-st.write("Debug - AgGrid response:", response)
-selected_rows = response.get('selected_rows', [])
-st.write("Debug - Geselecteerde rijen:", selected_rows)
-if selected_rows:
-    st.session_state.selected_rows = [r['_selectedRowNodeInfo']['nodeRowIndex'] for r in selected_rows if '_selectedRowNodeInfo' in r]
-else:
-    st.session_state.selected_rows = []
 
 # Knoppen toevoegen aan de GUI
 col1, col2 = st.columns(2)
@@ -251,19 +249,32 @@ with col1:
         # Voeg een lege rij toe aan het DataFrame
         new_row = pd.DataFrame({
             "Offertenummer": [None], "Artikelnaam": [""], "Artikelnummer": [""], "Breedte": [0], "Hoogte": [0],
-            "Aantal": [0], "RSP": [0], "M2 p/s": [0], "M2 totaal": [0], "Min_prijs": [0], "Max_prijs": [0]
+            "Aantal": [0], "RSP": [0], "M2 p/s": [0], "M2 totaal": [0]
         })
         st.session_state.offer_df = pd.concat([st.session_state.offer_df, new_row], ignore_index=True)
 
 with col2:
-    if st.button("Verwijder geselecteerde rij(en)"):
-        # Controleer of er geselecteerde rijen zijn voordat je de verwijderactie uitvoert
-        if st.session_state.selected_rows:
-            st.session_state.offer_df = delete_selected_rows(st.session_state.offer_df, st.session_state.selected_rows)
-            # Reset geselecteerde rijen na verwijdering
-            st.session_state.selected_rows = []
+    if st.button("Verwijder geselecteerde rijen", key='delete_rows_button'):
+        # Controleer of de geselecteerde rijen aanwezig zijn in de AgGrid edited_df_response
+        if 'selected_rows' in edited_df_response and isinstance(edited_df_response['selected_rows'], list):
+            selected_rows = edited_df_response['selected_rows']
         else:
-            st.warning("Selecteer eerst rijen om te verwijderen.")
+            selected_rows = []
+        
+        st.write("Geselecteerde rijen (debug informatie):", selected_rows)
+        
+        # Controleer of er daadwerkelijk rijen zijn geselecteerd voordat je verwijdert
+        if len(selected_rows) > 0:
+            try:
+                valid_selected_rows = [r['_selectedRowNodeInfo']['nodeRowIndex'] for r in selected_rows if '_selectedRowNodeInfo' in r]
+                st.write("Geldige geselecteerde rijen (debug informatie):", valid_selected_rows)  # Extra debug informatie
+                st.session_state.offer_df = delete_selected_rows(st.session_state.offer_df, valid_selected_rows)
+                st.session_state['trigger_update'] = True
+            except KeyError as e:
+                st.error(f"Een fout is opgetreden bij het verwerken van de geselecteerde rijen: {e}")
+        else:
+            st.warning("Er zijn geen rijen geselecteerd.")
+
 
 
 # Functie om getallen van 1 tot 100 te herkennen
@@ -615,32 +626,6 @@ if st.session_state.offer_df is not None and not st.session_state.offer_df.empty
 if 'Rijnummer' not in st.session_state.offer_df.columns:
     st.session_state.offer_df.insert(0, 'Rijnummer', range(1, len(st.session_state.offer_df) + 1))
     
-    # Maak grid-opties aan voor AgGrid zonder gebruik van JsCode
-gb = GridOptionsBuilder.from_dataframe(st.session_state.offer_df)
-gb.configure_default_column(flex=1, min_width=100, editable=True)
-gb.configure_column("Rijnummer", editable=False, cellStyle={"backgroundColor": "#f5f5f5"})
-gb.configure_column("Artikelnaam", width=400)  # Stel de kolombreedte van Artikelnaam in op 400 pixels
-gb.configure_column("Offertenummer", hide=True)
-gb.configure_column("Breedte", editable=True, type=["numericColumn"])
-gb.configure_column("Hoogte", editable=True, type=["numericColumn"])
-gb.configure_column("Aantal", editable=True, type=["numericColumn"])
-gb.configure_column("RSP", editable=False, type=["numericColumn"], cellStyle={"backgroundColor": "#f5f5f5"})
-gb.configure_selection('multiple', use_checkbox=True)
-gb.configure_grid_options(domLayout='normal', rowHeight=23)  # Dit zorgt ervoor dat scrollen mogelijk is
-
-grid_options = gb.build()
-
-# Toon de AG Grid met het material-thema
-edited_df_response = AgGrid(
-    st.session_state.offer_df,
-    gridOptions=grid_options,
-    theme='material',
-    fit_columns_on_grid_load=True,
-    enable_enterprise_modules=True,
-    update_mode='MANUAL',
-    columns_auto_size_mode=ColumnsAutoSizeMode.FIT_CONTENTS,
-    data_return_mode=DataReturnMode.AS_INPUT,
-)
 
 # Bewaar de wijzigingen die de gebruiker heeft aangebracht
 edited_df = edited_df_response['data']
@@ -652,16 +637,6 @@ if st.button("Bevestig wijzigingen", key='confirm_changes_button'):
             st.session_state.offer_df = update_rsp_for_all_rows(st.session_state.offer_df, prijsscherpte)
             st.session_state['trigger_update'] = True
           
-
-# Verwijder geselecteerde rijen
-if st.button("Verwijder geselecteerde rijen", key='delete_rows_button'):
-    selected = edited_df_response['selected_rows'] if edited_df_response['selected_rows'] is not None else []
-    st.write("Geselecteerde rijen (debug informatie):", selected)
-    if selected is not None and len(selected) > 0:
-        st.session_state.offer_df = delete_selected_rows(st.session_state.offer_df, selected)
-    st.session_state['trigger_update'] = True
-
-
 # Voeg een knop toe om de offerte als PDF te downloaden
 if st.button("Download offerte als PDF", key='download_pdf_button'):
     pdf_buffer = generate_pdf(st.session_state.offer_df)
