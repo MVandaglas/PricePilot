c27a78f20d8d1362a4ee4a13d05f74d99674696c
MVandaglas <martijn.wissink@vandaglas.com>
2024-12-02 13:09:37 +0100

    Update PricePilot.py

--- PricePilot.py
+++ PricePilot.py
@@ -226,12 +226,10 @@ edited_df_response = AgGrid(
 
 
 # Sla de geselecteerde rijen op in sessie status
-selected_rows = edited_df_response.get('selected', [])
+selected_rows = edited_df_response.get('selectedRows', [])  # Pas de key aan voor het ophalen van geselecteerde rijen
 
-# Zorg dat selected_rows geen None of DataFrame is, maar altijd een lijst
-if isinstance(selected_rows, pd.DataFrame):
-    selected_rows = selected_rows.index.tolist()
-elif selected_rows is None:
+# Verwijder oude typecontroles, deze zijn niet langer nodig
+if selected_rows is None or not isinstance(selected_rows, list):
     selected_rows = []
 
 # Controleer vervolgens op de lengte van de lijst
@@ -245,6 +243,7 @@ else:
 
 # Debugging om te controleren welke gegevens er in selected_rows zitten
 st.write("Debug - Type selected_rows:", type(selected_rows))
+st.write("Debug - Inhoud edited_df_response:", edited_df_response)  # Voeg extra debugging toe
 st.write("Debug - Geselecteerde rijen:", selected_rows)
 
 # Als er rijen zijn geselecteerd, zet deze in de sessie state
