26631d43e8fb8cbb5e4a8078fd440f23d3aa112d
MVandaglas <martijn.wissink@vandaglas.com>
2024-12-13 12:00:39 +0100

    Update PricePilot.py

--- PricePilot.py
+++ PricePilot.py
@@ -61,7 +61,7 @@ selected_tab = st.radio(
 )
 
 if st.session_state.offer_df is None or st.session_state.offer_df.empty:
-    st.session_state.offer_df = pd.DataFrame(columns=["Offertenummer", "Artikelnaam", "Artikelnummer", "Spacer", "Breedte", "Hoogte", "Aantal", "RSP", "SAP prijs" "M2 p/s", "M2 totaal", "Min_prijs", "Max_prijs", "Verkoopprijs", "Prijs_backend"])
+    st.session_state.offer_df = pd.DataFrame(columns=["Rijnummer", "Offertenummer", "Artikelnaam", "Artikelnummer", "Spacer", "Breedte", "Hoogte", "Aantal", "RSP", "SAP prijs", "M2 p/s", "M2 totaal", "Min_prijs", "Max_prijs", "Verkoopprijs", "Prijs_backend"])
 
 
 # Omzetting naar numerieke waarden en lege waarden vervangen door 0
