72d223eec72661bcb5d97112b1a6d7c48e744954
MVandaglas <martijn.wissink@vandaglas.com>
2024-12-17 15:24:58 +0100

    Update PricePilot.py

--- PricePilot.py
+++ PricePilot.py
@@ -423,10 +423,10 @@ else:
 
 # Maak grid-opties aan voor AgGrid met gebruik van een "select all" checkbox in de header
 gb = GridOptionsBuilder.from_dataframe(st.session_state.offer_df)
-gb.configure_default_column(flex=1, min_width=100, editable=True)
+gb.configure_default_column(flex=1, min_width=80, editable=True)
 gb.configure_column("Spacer", editable=True, cellEditor='agSelectCellEditor', cellEditorParams={"values": ["4 - alu", "6 - alu", "7 - alu", "8 - alu", "9 - alu", "10 - alu", "12 - alu", "13 - alu", "14 - alu", "15 - alu", "16 - alu", "18 - alu", "20 - alu", "24 - alu", "10 - warm edge", "12 - warm edge", "14 - warm edge", "15 - warm edge", "16 - warm edge", "18 - warm edge", "20 - warm edge", "24 - warm edge"]})
 gb.configure_column("Rijnummer", type=["numericColumn"], editable=False, cellStyle={"backgroundColor": "#e0e0e0"})
-gb.configure_column("Artikelnaam", width=600)  # Stel de kolombreedte van Artikelnaam in op 400 pixels
+gb.configure_column("Artikelnaam", width=600)
 gb.configure_column("Offertenummer", hide=True)
 gb.configure_column("Prijs_backend", hide=False)
 gb.configure_column("Min_prijs", hide=True)
