88f8b7adc68952e84bbaf99a0c539355af519ac7
MVandaglas <martijn.wissink@vandaglas.com>
2025-01-08 11:23:39 +0100

    Update PricePilot.py

--- PricePilot.py
+++ PricePilot.py
@@ -610,7 +610,7 @@ with tab1:
         theme='streamlit',
         fit_columns_on_grid_load=False,
         enable_enterprise_modules=True,
-        update_mode=GridUpdateMode.VALUE_CHANGED,
+        update_mode=GridUpdateMode.MODEL_CHANGED,
         columns_auto_size_mode=ColumnsAutoSizeMode.FIT_CONTENTS,
         allow_unsafe_jscode=True
     )
