66075f7098d6bff5a8f11c0c9a5507b18e3c3e27
MVandaglas <martijn.wissink@vandaglas.com>
2025-01-10 15:10:34 +0100

    Update PricePilot.py

--- PricePilot.py
+++ PricePilot.py
@@ -612,16 +612,7 @@ with tab1:
     )
 
     
-    # Update de DataFrame na elke wijziging
-    if "data" in edited_df_response:
-        updated_df = pd.DataFrame(edited_df_response['data'])
-        # Werk de sessiestatus bij met de nieuwe data
-        st.session_state.offer_df = updated_df
-        # Voer alle benodigde berekeningen uit
-        st.session_state.offer_df = update_offer_data(st.session_state.offer_df)
-        st.session_state.offer_df = bereken_prijs_backend(st.session_state.offer_df)
-
-
+   
 
 # Verbeterde update_tabel functie
 def update_tabel():
