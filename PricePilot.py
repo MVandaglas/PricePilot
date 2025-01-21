c00c2d6993088ac92d2cd44106297aa62eb916c1
MVandaglas <martijn.wissink@vandaglas.com>
2025-01-21 13:18:00 +0100

    Update PricePilot.py

--- PricePilot.py
+++ PricePilot.py
@@ -18,7 +18,7 @@ from PyPDF2 import PdfReader
 import extract_msg
 import pdfplumber
 from functools import partial
-from database_setup import create_connection
+from setup_database import create_connection
 import sqlite3
 
 # OpenAI API-sleutel instellen
