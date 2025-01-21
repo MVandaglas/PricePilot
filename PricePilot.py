03885e30950ed1eea41d942d76efe67a0651a320
MVandaglas <martijn.wissink@vandaglas.com>
2025-01-21 15:46:46 +0100

    Update PricePilot.py

--- PricePilot.py
+++ PricePilot.py
@@ -208,6 +208,7 @@ with col1:
     customer_input = st.sidebar.text_area("Voer hier het klantverzoek in (e-mail, tekst, etc.)")
     customer_number = st.sidebar.text_input("Klantnummer (6 karakters)", max_chars=6)
     st.session_state.customer_number = str(customer_number) if customer_number else ''
+    customer_reference = st.sidebar.text_input("Klantreferentie")
     offer_amount = totaal_bedrag
     
     
