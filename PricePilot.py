6229be554bdfd7765c23aeebf236ce18a26225c2
MVandaglas <martijn.wissink@vandaglas.com>
2025-01-24 13:42:18 +0100

    Update PricePilot.py

--- PricePilot.py
+++ PricePilot.py
@@ -265,7 +265,10 @@ with tab3:
     customer_input = st.sidebar.text_area("Voer hier het klantverzoek in (e-mail, tekst, etc.)")
     customer_number = st.sidebar.text_input("Klantnummer (6 karakters)", max_chars=6)
     st.session_state.customer_number = str(customer_number) if customer_number else ''
-    customer_reference = st.sidebar.text_input("Klantreferentie", value=st.session_state.get("customer_reference", "") if not st.session_state.get("customer_reference", "").strip() else "")
+    customer_reference = st.sidebar.text_input("Klantreferentie", value=st.session_state.get("customer_reference", ""))
+    if not customer_reference.strip():
+    st.session_state["customer_reference"] = ""
+
 
 
     offer_amount = totaal_bedrag
