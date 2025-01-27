30eca17c7899bb14c97f70d8fcf3bbf94c5ca085
MVandaglas <martijn.wissink@vandaglas.com>
2025-01-27 16:30:56 +0100

    Update PricePilot.py

--- PricePilot.py
+++ PricePilot.py
@@ -552,7 +552,7 @@ with col2:
                     break
         
         # Toon klantinformatie als metrics
-        st.metric("Omzet klant", omzet_klant)
+        st.write(f"**Omzet klant:** € {omzet_klant:,.2f}")
         st.metric("Klantgrootte", klantgrootte)
         st.metric("Prijsscherpte", prijsscherpte)
 
