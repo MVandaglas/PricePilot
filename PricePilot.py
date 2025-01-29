f2208e92f1f86e04f25b016c6603613a5ba9997f
MVandaglas <martijn.wissink@vandaglas.com>
2025-01-29 11:25:32 +0100

    Update PricePilot.py

--- PricePilot.py
+++ PricePilot.py
@@ -2208,7 +2208,8 @@ with tab3:
     
     # Rechterkolom: Excel-file uploader in een expander
     with col2:
-        st.markdown("###Upload synoniemen 🧍‍♂⬌🧍‍♂️")
+        st.markdown("### Upload synoniemen 🧍‍♂⬌🧍‍♂️")
+       
         with st.expander("Upload Synoniemen via Excel ✨"):
             st.markdown("Upload een Excel-bestand met twee kolommen: **Artikelnummer** en **Synoniem**.")
     
