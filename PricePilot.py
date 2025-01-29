0b01f54b1b31202ae0ce01bcd614cb9d4e7d9cc9
MVandaglas <martijn.wissink@vandaglas.com>
2025-01-29 13:15:45 +0100

    Update PricePilot.py

--- PricePilot.py
+++ PricePilot.py
@@ -1147,6 +1147,7 @@ def handle_gpt_chat():
                 else:
                     article_number = m2_match.group(3)
                     m2_total = int(m2_match.group(4))
+                    st.write(article_number)
                 
                 # Update `current_article_number` met het gevonden artikelnummer
                 current_article_number = synonym_dict.get(article_number, article_number)
