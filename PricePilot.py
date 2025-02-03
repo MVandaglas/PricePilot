9db30dcbbd981a7e38b6b8715fab28e6de082c53
MVandaglas <martijn.wissink@vandaglas.com>
2025-02-03 14:09:20 +0100

    Update PricePilot.py

--- PricePilot.py
+++ PricePilot.py
@@ -36,6 +36,7 @@ CLIENT_ID = st.secrets.get("SP_CLIENTID")
 CLIENT_SECRET = st.secrets.get("SP_CLIENTSECRET")
 SP_SITE = st.secrets.get("SP_SITE")
 
+st.write(secrets)
 
 if not SP_SITE:
     st.error("❌ Fout: SP_SITE is niet correct geladen! Controleer je Streamlit secrets.")
