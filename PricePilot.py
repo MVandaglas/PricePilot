d0ad32a4d831627b7c8cc1bd0b3f49b04836b4b2
MVandaglas <martijn.wissink@vandaglas.com>
2025-02-04 16:15:02 +0100

    Update PricePilot.py

--- PricePilot.py
+++ PricePilot.py
@@ -1284,8 +1284,8 @@ def handle_gpt_chat():
             st.session_state.offer_df = update_rsp_for_all_rows(st.session_state.offer_df, prijsscherpte)
             st.session_state["trigger_update"] = True
             st.session_state.offer_df = reset_rijnummers(st.session_state.offer_df)
-            update_tabel()
             st.rerun()
+            update_tabel()
         else:
             st.sidebar.warning("Geen gegevens gevonden om toe te voegen.")
     elif customer_file:
