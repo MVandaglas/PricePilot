3348452ec8b601c85fd1d8f565034ab9fdb59dd6
MVandaglas <martijn.wissink@vandaglas.com>
2025-02-18 13:59:32 +0100

    Update PricePilot.py

--- PricePilot.py
+++ PricePilot.py
@@ -1583,7 +1583,7 @@ def extract_pdf_to_dataframe(pdf_reader):
 
             # Controleer of de regel "Aantal", "Breedte" of "Hoogte" bevat en sla deze over vanaf regel 3
             if idx > 2 and re.search(r"\b(Aantal|Breedte|Hoogte)\b:?", line, re.IGNORECASE):
-            continue
+                continue
 
                 
             # Splits de kolommen op basis van >3 spaties of tabs, en negeer komma's als scheidingsteken
