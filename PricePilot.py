85057724ceab3d4dfade20fec7a73136dfa05bfa
MVandaglas <martijn.wissink@vandaglas.com>
2025-02-19 10:11:05 +0100

    Update PricePilot.py

--- PricePilot.py
+++ PricePilot.py
@@ -1672,7 +1672,6 @@ def extract_pdf_to_dataframe(pdf_reader):
                     if st.button(f"Verwerk batch {st.session_state.batch_number + 1}", key=f"batch_{st.session_state.batch_number}"):
                         st.session_state["df_current"] = df_backlog.copy()  # Zet backlog als nieuwe dataset
                         st.session_state["batch_number"] += 1  # Verhoog batchnummer
-                        st.rerun()  # Voorkomt duplicatie
                 else:
                     st.success("🎉 Alle batches zijn verwerkt! Geen achtergehouden regels meer.")
                     st.session_state.df_current = pd.DataFrame()  # Reset UI
