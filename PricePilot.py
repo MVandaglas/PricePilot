5d332500fe2e674de8e0c7b52a1e26642781a301
MVandaglas <martijn.wissink@vandaglas.com>
2025-03-05 16:31:30 +0100

    Update PricePilot.py

--- PricePilot.py
+++ PricePilot.py
@@ -807,7 +807,17 @@ gb = GridOptionsBuilder.from_dataframe(st.session_state.offer_df)
 gb.configure_default_column(flex=1, minWidth=50, editable=True)
 gb.configure_column("Spacer", editable=True, cellEditor='agSelectCellEditor', cellEditorParams={"values": ["4 - alu", "6 - alu", "7 - alu", "8 - alu", "9 - alu", "10 - alu", "12 - alu", "13 - alu", "14 - alu", "15 - alu", "16 - alu", "18 - alu", "20 - alu", "24 - alu", "10 - warm edge", "12 - warm edge", "14 - warm edge", "15 - warm edge", "16 - warm edge", "18 - warm edge", "20 - warm edge", "24 - warm edge"]})
 gb.configure_column("Rijnummer", type=["numericColumn"], editable=False, cellStyle={"backgroundColor": "#e0e0e0"}, cellRenderer=cell_renderer_js)
-gb.configure_column("Artikelnaam", width=600)
+gb.configure_column(
+    "Artikelnaam",
+    editable=True,
+    cellEditor="agRichSelectCellEditor",
+    cellEditorParams={
+        "values": list(article_mapping.keys()),  # De mogelijke waarden
+        "searchable": True,  # Laat je typen in de dropdown
+        "suppressKeyboardEvent": False  # Zorgt dat je kunt typen zonder dat de dropdown sluit
+    },
+    width=600
+)
 gb.configure_column("Offertenummer", hide=True)
 gb.configure_column("Prijs_backend", hide=True)
 gb.configure_column("Min_prijs", hide=True)
