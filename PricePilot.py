7367f98e7797563d05989c5ddfb6bc9ef36b40a2
MVandaglas <martijn.wissink@vandaglas.com>
2025-03-14 15:42:43 +0100

    Update PricePilot.py

--- PricePilot.py
+++ PricePilot.py
@@ -333,7 +333,7 @@ with tab4:
                 
                # **Batch insert voor nieuwe data in kleinere groepen**
                 nieuwe_data = df[df["nieuw"]].drop(columns=["nieuw", "update_nodig", "huidige_prijs"])
-                batch_size = 2000  # Max 5.000 rijen per batch
+                batch_size = 11000  # Max 5.000 rijen per batch
                 if not nieuwe_data.empty:
                     for i in range(0, len(nieuwe_data), batch_size):
                         nieuwe_data.to_sql("SAP_prijzen", engine, if_exists="append", index=False, method="multi", chunksize=1000)
@@ -365,7 +365,7 @@ with tab4:
                         data_tuples = [tuple(row) for row in nieuwe_data.itertuples(index=False, name=None)]
                         
                         # **Verwerk in batches om SQL Server limiet te vermijden**
-                        batch_size = 2000
+                        batch_size = 11000
                         for i in range(0, len(data_tuples), batch_size):
                             cursor.executemany(insert_query, data_tuples[i:i+batch_size])
                             conn.commit()
