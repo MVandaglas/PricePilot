49dfed1c66aa998059c55b9744a4f8bea1ae83a4
MVandaglas <martijn.wissink@vandaglas.com>
2025-03-18 15:58:37 +0100

    Update PricePilot.py

--- PricePilot.py
+++ PricePilot.py
@@ -1243,7 +1243,7 @@ def handle_gpt_chat():
 
         for line in lines:
             # Verbeterde regex om volledige glassamenstellingen te vinden
-            detected_article_number = re.search(r'([A-Za-z0-9/]+(?:\s*[-/*#]\s*[A-Za-z0-9/.]+)*)', line)
+            detected_article_number = re.search(r'([A-Za-z0-9/.]+(?:\s*[-/*#]\s*[A-Za-z0-9/.]+)*)', line)
             
             if detected_article_number:
                 current_article_number = detected_article_number.group(0).replace(" ", "")  # Spaties verwijderen
