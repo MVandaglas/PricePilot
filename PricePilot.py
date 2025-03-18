a49366c107ea97015abbd8af0fc9d290a8bb47ad
MVandaglas <martijn.wissink@vandaglas.com>
2025-03-18 15:10:58 +0100

    Update PricePilot.py

--- PricePilot.py
+++ PricePilot.py
@@ -200,7 +200,7 @@ with tab4:
                 
                         if tabel_bestaat:
                             # **Haal de geaccordeerde synoniemen op**
-                            cursor.execute("SELECT Artikelnummer, Artikelnaam, Synoniem FROM SynoniemenAI")
+                            cursor.execute("SELECT Artikelnummer, Synoniem FROM SynoniemenAI")
                             synoniemen_data = cursor.fetchall()
                             
                             # **Haal de kolomnamen op**
@@ -246,17 +246,16 @@ with tab4:
                                                 if isinstance(rij, dict):
                                                     synoniem = rij.get("Synoniem")
                                                     artikelnummer = rij.get("Artikelnummer")
-                                                    artikelnaam = rij.get("Artikelnaam")
                                                 elif isinstance(rij, (tuple, list)) and len(rij) == 2:
-                                                    artikelnummer, artikelnaam, synoniem = rij  # Pak waarden uit tuple/lijst
+                                                    artikelnummer, synoniem = rij  # Pak waarden uit tuple/lijst
                                                 else:
                                                     st.warning(f"Ongeldig formaat van rij: {rij}")
                                                     continue
                                 
                                                 if synoniem and artikelnummer:
                                                     cursor.execute("""
-                                                    DELETE FROM SynoniemenAI WHERE Artikelnummer = ? AND Synoniem = ? And Artikelnaam = ?;
-                                                    """, (artikelnummer, artikelnaam, synoniem))
+                                                    DELETE FROM SynoniemenAI WHERE Artikelnummer = ? AND Synoniem = ?;
+                                                    """, (artikelnummer, synoniem))
                                 
                                             conn.commit()
                                             st.success("Geselecteerde synoniemen zijn verwijderd uit 'synoniemen'.")
